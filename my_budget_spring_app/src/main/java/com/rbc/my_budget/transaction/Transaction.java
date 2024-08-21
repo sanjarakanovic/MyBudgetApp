d4077bdc869dad06a785809d7bca97caa7454175
sanja.rakanovic <sanja.rakanovic9@gmail.com>
2024-08-21 17:26:04 +0200

    added controllers, services, repositories, entities

--- my_budget_spring_app/src/main/java/com/rbc/my_budget/transaction/Transaction.java
+++ my_budget_spring_app/src/main/java/com/rbc/my_budget/transaction/Transaction.java
@@ -13,7 +13,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.extern.slf4j.Slf4j;
 
 import java.math.BigDecimal;
 
